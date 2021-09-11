29a951c38fa6a3ddbbbe896965ccb869b0d636aa
chinhdz97 <kqtchcheng97@gmail.com>
2021-09-11 16:59:23 +0700

    import dependencies

--- app/src/App.js
+++ app/src/App.js
@@ -1,12 +1,22 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { Howl } from 'howler';
+import sourdURL from './assets/botayxuongbanoi.mp3'
+// const mobilenet = require('@tensorflow-models/mobilenet');
+// const knnClassifier = require('@tensorflow-models/knn-classifier');
+
+var sound = new Howl({
+  src: [sourdURL]
+});
+
+sound.play();
 
 function App() {
   return (
     <div className="app">
       <video
-      className="video"
-      autoPlay
+        className="video"
+        autoPlay
       />
       <div className="control">
         <button className="btn">Train 1</button>
