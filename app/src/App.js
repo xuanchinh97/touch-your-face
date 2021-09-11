9d45228d2f8a338abb9d9434a35d3299f384aadd
chinhdz97 <kqtchcheng97@gmail.com>
2021-09-11 21:08:29 +0700

    train() and run()

--- app/src/App.js
+++ app/src/App.js
@@ -1,6 +1,10 @@
 import React, { useEffect, useRef } from 'react';
 import './App.css';
 import { Howl } from 'howler';
+import '@tensorflow/tfjs-backend-cpu';
+import * as tf from '@tensorflow/tfjs'
+import * as mobilenet from '@tensorflow-models/mobilenet'
+import * as knnClassifier from '@tensorflow-models/knn-classifier'
 import sourdURL from './assets/botayxuongbanoi.mp3'
 // const mobilenet = require('@tensorflow-models/mobilenet');
 // const knnClassifier = require('@tensorflow-models/knn-classifier');
@@ -11,12 +15,27 @@ var sound = new Howl({
 
 sound.play();
 
+const NOT_TOUCH_LABEL = 'not_touch'
+const TOUCHED_LABEL = 'touched'
+const TRAINING_TIMES = 50
+const TOUCHED_CONFIDENCE = 0.8
+
 function App() {
   const video = useRef();
+  const model = useRef();
+  const classifier = useRef();
 
   const init = async () => {
     console.log('init...');
     await setupCamera();
+    console.log('set up camera success');
+
+    model.current = await mobilenet.load();
+
+    classifier.current = knnClassifier.create();
+
+    console.log('set up done');
+
   }
 
   const setupCamera = () => {
@@ -35,6 +54,45 @@ function App() {
       }
     })
   }
+
+  const train = async label => {
+    console.log(`[${label}] đang training`)
+    for (let i = 0; i < TRAINING_TIMES; i++) {
+      console.log(`progress ${parseInt((i + 1) / TRAINING_TIMES * 100)}%`);
+
+      await training(label)
+    }
+  }
+
+  const training = label => {
+    return new Promise(async resolve=> {
+      const embedding =model.current.infer(video.current, true)
+      classifier.current.addExample(embedding,label)
+      await sleep(100)
+      resolve()
+    })
+  }
+
+  const run = async () => {
+    const embedding =model.current.infer(video.current, true)
+    const result = await classifier.current.predictClass(embedding)
+    console.log('label: ', result.label)
+    console.log('confidences: ', result.confidences)
+
+    if(result.label === TOUCHED_LABEL && result.confidences[result.label] > TOUCHED_CONFIDENCE) {
+
+    } else {
+      
+    }
+
+    await sleep(200)
+    run()
+
+  }
+
+const sleep = (ms = 0) => {
+  return new Promise(resolve => setTimeout(resolve, ms))
+}
 
   useEffect(() => {
     init();
@@ -55,9 +113,9 @@ function App() {
         autoPlay
       />
       <div className="control">
-        <button className="btn">Train 1</button>
-        <button className="btn">Train 2</button>
-        <button className="btn">Run</button>
+        <button className="btn" onClick={() => train(NOT_TOUCH_LABEL)}>Train 1</button>
+        <button className="btn" onClick={() => train(TOUCHED_LABEL)}>Train 2</button>
+        <button className="btn" onClick={() => run()}>Run</button>
       </div>
     </div>
   );
