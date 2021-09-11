5f6c5bd909636600de5a8edd50d4e9d5381ef143
chinhdz97 <kqtchcheng97@gmail.com>
2021-09-11 17:15:52 +0700

    setup camera

--- app/src/App.js
+++ app/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import './App.css';
 import { Howl } from 'howler';
 import sourdURL from './assets/botayxuongbanoi.mp3'
@@ -12,9 +12,45 @@ var sound = new Howl({
 sound.play();
 
 function App() {
+  const video = useRef();
+
+  const init = async () => {
+    console.log('init...');
+    await setupCamera();
+  }
+
+  const setupCamera = () => {
+    return new Promise((resolve, reject) => {
+      navigator.getUserMedia = navigator.getUserMedia || navigator.webketGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
+
+      if (navigator.getUserMedia) {
+        navigator.getUserMedia({
+          video: true
+        }, stream => {
+          video.current.srcObject = stream;
+          video.current.addEventListener('loadeddata', resolve)
+        }, error => reject(error))
+      } else {
+        reject();
+      }
+    })
+  }
+
+  useEffect(() => {
+    init();
+
+
+    // cleanup
+    return () => {
+
+    }
+  }, [])
+
+
   return (
     <div className="app">
       <video
+        ref={video}
         className="video"
         autoPlay
       />
