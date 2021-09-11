28e4178b5b5ac770faadf8eda60b0b34c4bced71
chinhdz97 <kqtchcheng97@gmail.com>
2021-09-11 16:39:50 +0700

    build UI

--- app/src/App.js
+++ app/src/App.js
@@ -4,8 +4,15 @@ import './App.css';
 function App() {
   return (
     <div className="app">
-      
-      
+      <video
+      className="video"
+      autoPlay
+      />
+      <div className="control">
+        <button className="btn">Train 1</button>
+        <button className="btn">Train 2</button>
+        <button className="btn">Run</button>
+      </div>
     </div>
   );
 }
